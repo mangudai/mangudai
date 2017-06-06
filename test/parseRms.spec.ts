e31c465973abd26282b2f3e8a6a0a83f9f9d4068
Nikita Litvin <n@deltaidea.com>
2017-06-06 18:59:24 +0500

    chore(test/parseRms): assert parse errors before AST for debugging
    AST is `undefined` when something goes wrong. It's more informative to output the parse errors instead of the AST when they don't match expectations.

--- test/parseRms.spec.ts
+++ test/parseRms.spec.ts
@@ -8,43 +8,43 @@ const readSample = (name: string) => readFileSync(join(__dirname, 'samples', `${
 describe('parseRms', () => {
   it('returns errors on illegal tokens', () => {
     const { ast, errors } = parseRms('=== wtf is this even ===')
-    expect(ast).to.equal(undefined)
     expect(errors).to.not.deep.equal([])
+    expect(ast).to.equal(undefined)
   })
 
   it('returns CST visitor errors instead of throwing them', () => {
     const { ast, errors } = parseRms('<PLAYER_SETUP> whatever { #define }')
-    expect(ast).to.equal(undefined)
     expect(errors).to.not.deep.equal([])
+    expect(ast).to.equal(undefined)
   })
 
   it('works on empty input', () => {
     const { ast, errors } = parseRms('')
-    expect(ast).to.be.an.instanceOf(Object)
     expect(errors).to.deep.equal([])
+    expect(ast).to.be.an.instanceOf(Object)
   })
 
   it('works on spaces', () => {
     const { ast, errors } = parseRms('   \n\n   \n \t ')
-    expect(ast).to.be.an.instanceOf(Object)
     expect(errors).to.deep.equal([])
+    expect(ast).to.be.an.instanceOf(Object)
   })
 
   it('works on comments', () => {
     const { ast, errors } = parseRms('/* comment one */ /* comment two */')
-    expect(ast).to.be.an.instanceOf(Object)
     expect(errors).to.deep.equal([])
+    expect(ast).to.be.an.instanceOf(Object)
   })
 
   it('works on sections', () => {
     const { ast, errors } = parseRms(readSample('sections'))
-    expect(ast).to.be.an.instanceOf(Object)
     expect(errors).to.deep.equal([])
+    expect(ast).to.be.an.instanceOf(Object)
   })
 
   it('works on #const and #define, both global and in sections', () => {
     const { ast, errors } = parseRms(readSample('const-and-define'))
-    expect(ast).to.be.an.instanceOf(Object)
     expect(errors).to.deep.equal([])
+    expect(ast).to.be.an.instanceOf(Object)
   })
 })
