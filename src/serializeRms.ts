b42a7f82c52af1e6baddb1866a4894cbe7fe8d82
Nikita Litvin <n@deltaidea.com>
2017-06-07 13:56:09 +0500

    feat(serializeRms): add support for conditions

--- src/serializeRms.ts
+++ src/serializeRms.ts
@@ -1,21 +1,41 @@
-import { RmsAst, RmsTopLevelStatement, RmsSectionStatement, RmsAttribute, RmsSection, RmsCommand,
-  RmsConstDefinition, RmsFlagDefinition } from './parseRms'
+import { RmsAst, RmsIf, RmsTopLevelStatement, RmsSection, RmsSectionStatement,
+    RmsCommand, RmsCommandStatement, RmsAttribute, RmsConstDefinition, RmsFlagDefinition } from './parseRms'
 
-type RmsAstNode = RmsAst | RmsTopLevelStatement | RmsSectionStatement | RmsAttribute
+type RmsAstNode = RmsAst | RmsTopLevelStatement | RmsSectionStatement | RmsCommandStatement
 
 const serializers: { [x: string]: (n: RmsAstNode) => string } = {
   RandomMapScript: (ast: RmsAst) => ast.statements.map(serializeNode).join('\n\n') + '\n',
+
   Section: ({ name, statements}: RmsSection) =>
     `<${name}>\n` +
     statements.map(serializeNode).map(indent).join('\n'),
-  Command: ({ name, value, attributes }: RmsCommand) =>
+
+  Command: ({ name, value, statements }: RmsCommand) =>
     name +
     (value === undefined ? '' : ' ' + value) +
-    (attributes === undefined ? '' :
-      attributes.length ? ' {\n' + attributes.map(serializeNode).map(indent).join('\n') + '\n}' : ' {}'),
+    (statements === undefined ? '' :
+      statements.length ? ' {\n' + statements.map(serializeNode).map(indent).join('\n') + '\n}' : ' {}'),
+
   Attribute: ({ name, value }: RmsAttribute) => name + (value === undefined ? '' : ' ' + value),
+
   ConstDefinition: ({ name, value }: RmsConstDefinition) => '#const ' + name + ' ' + value,
-  FlagDefinition: ({ flag }: RmsFlagDefinition) => `#define ${flag}`
+
+  FlagDefinition: ({ flag }: RmsFlagDefinition) => `#define ${flag}`,
+
+  If: ({ condition, statements, elseifs, elseStatements }: RmsIf<any>) => {
+    let str = `if ${condition}`
+    if (statements && statements.length) str += '\n' + statements.map(serializeNode).map(indent).join('\n')
+    if (elseifs && elseifs.length) str += '\n' + elseifs.map(serializeElseif).join('\n')
+    if (elseStatements && elseStatements.length) str += '\nelse\n' + elseStatements.map(serializeNode).map(indent).join('\n')
+    str += '\nendif'
+    return str
+  }
+}
+
+function serializeElseif ({ condition, statements }: { condition: string, statements: RmsAstNode[] }) {
+  let str = `elseif ${condition}`
+  if (statements && statements.length) str += '\n' + statements.map(serializeNode).map(indent).join('\n')
+  return str
 }
 
 function indent (str: string): string {
