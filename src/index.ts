e36e464115c2f0847ef20d3e871df85434ca904d
Nikita Litvin <n@deltaidea.com>
2017-07-13 20:49:49 +0500

    chore(serializeRms): remove serialize feature to tighten package scope
    Related to #9.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { TextSpan } from './tokenHelpers'
 
 export * from './parseRms'
-export { serialize } from './serializeRms'
 export { lint } from './lintRms/index'
 
 export interface TextSpanError extends Error {
--- test/samples/generated/generateSamples.ts
+++ test/samples/generated/generateSamples.ts
@@ -1,11 +1,10 @@
 /**
- * Run this file with `ts-node` to generate `.ast` files in this directory from `.rms` samples.
+ * Run this file with `ts-node` to generate files in this directory from `.rms` samples.
  */
 
 import { readdirSync, readFileSync, writeFileSync } from 'fs'
 import { basename, extname, join, resolve } from 'path'
 import { parse } from '../../../src/parseRms'
-import { serialize } from '../../../src/serializeRms'
 import { lint } from '../../../src/lintRms'
 
 const samples = readdirSync(resolve(__dirname, '..'))
@@ -23,8 +22,4 @@ samples.forEach(filename => {
   const lintErrors = JSON.stringify(result.ast ? lint(result.ast) : [], null, 2)
   const lintFilename = basename(filename, '.rms') + '.lint-errors.json'
   writeFileSync(join(__dirname, lintFilename), lintErrors + '\n')
-
-  const generatedRms = result.ast ? serialize(result.ast) : ''
-  const generatedRmsFilename = basename(filename, '.rms') + '.generated.rms'
-  writeFileSync(join(__dirname, generatedRmsFilename), generatedRms)
 })
