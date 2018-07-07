425c1404bf025009f0f95315c199ead2dff82775
Nikita Litvin <n@deltaidea.com>
2018-07-07 23:18:06 +0300

    fix(lint/no-redeclare): allow similar declarations in branches
    Related to mangudai/vscode#1

--- src/lint/rules/no-redeclare.ts
+++ src/lint/rules/no-redeclare.ts
@@ -1,18 +1,70 @@
 import { LintError } from '../'
-import { Token, Script } from '../../parse'
-import { getNodes, getToken } from '../../treeHelpers'
+import { Token, Script, AstNode, DeclarationStatement, IfStatement, RandomStatement,
+  ChanceStatement, Statement } from '../../parse'
+import { getToken } from '../../treeHelpers'
 import { getBoundaries } from '../../tokenHelpers'
 
+type SeenIdentifiers = { [x: string]: boolean }
+
 export function check (ast: Script): LintError[] {
-  const seenIdentifiers: { [x: string]: boolean } = {}
+  // const seenIdentifiers: { [x: string]: boolean } = {}
   const redeclared: Token[] = []
 
-  const nodes = getNodes(ast, 'DeclarationStatement')
-  nodes.forEach(node => {
+  function reduceStatementsToSeen (statements: Statement[] | undefined, seen: SeenIdentifiers): SeenIdentifiers {
+    return (statements && statements.length)
+      ? statements.reduce((acc, statement) => ({ ...acc, ...visitNode(statement, acc) }), seen)
+      : seen
+  }
+
+  function visitIfNode (node: IfStatement, seen: SeenIdentifiers) {
+    const seenInIfBranch = reduceStatementsToSeen(node.statements, seen)
+    const seenInElseIfBranches = !node.elseifs
+      ? seen
+      : node.elseifs
+        .map(elseIf => reduceStatementsToSeen(elseIf.statements, seen))
+        .reduce((acc, next) => ({ ...acc, ...next }), seen)
+    const seenInElseBranch = reduceStatementsToSeen(node.elseStatements, seen)
+
+    return {
+      ...seenInIfBranch,
+      ...seenInElseIfBranches,
+      ...seenInElseBranch
+    }
+  }
+
+  function visitRandomNode (node: RandomStatement, seen: SeenIdentifiers) {
+    return node.statements
+      .filter(s => s.type === 'ChanceStatement')
+      .map(chance => reduceStatementsToSeen((chance as ChanceStatement).statements, seen))
+      .reduce((acc, next) => ({ ...acc, ...next }), seen)
+  }
+
+  function visitDeclarationNode (node: DeclarationStatement, seen: SeenIdentifiers) {
     const nameToken = getToken(node, 'identifier', true)
-    if (seenIdentifiers[nameToken.value]) redeclared.push(nameToken)
-    else seenIdentifiers[nameToken.value] = true
-  })
+
+    if (seen[nameToken.value]) {
+      redeclared.push(nameToken)
+      return seen
+    } else {
+      return { ...seen, [nameToken.value]: true }
+    }
+  }
+
+  function visitNode (node: AstNode, seen: SeenIdentifiers) {
+    if (node.type === 'IfStatement') {
+      return visitIfNode(node as IfStatement, seen)
+    } else if (node.type === 'RandomStatement') {
+      return visitRandomNode(node as RandomStatement, seen)
+    } else if (node.type === 'DeclarationStatement') {
+      return visitDeclarationNode(node as DeclarationStatement, seen)
+    } else if ('statements' in (node as Statement)) {
+      return reduceStatementsToSeen((node as any).statements, seen)
+    } else {
+      return seen
+    }
+  }
+
+  visitNode(ast, {})
 
   return redeclared.map(x => ({
     name: 'LintError',
